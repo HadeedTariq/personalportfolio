63b207ce3d07f5382240f52136af4d294b6978a2
HadeedTariq <computeranalog351@gmail.com>
2023-03-27 10:47:19 +0500

    add one more preoject in my portfolio

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Button } from '../constants'
 import '../Css/Home.css'
 function Home() {
 
@@ -13,9 +12,6 @@ function Home() {
            <p>I am a front end developer working in <span className='passion'>Html || Css || JavaScript and React</span></p>
            <p>I specialize turning design concept into functional, responsive websites and web application</p>
            <button className='cv-btn'>Download Cv</button>
-         </div>
-         <div className="logo">
-          <img src="https://images.pexels.com/photos/12812705/pexels-photo-12812705.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
          </div>
         </div>
        </div>
--- src/constants/ProjectData.js
+++ src/constants/ProjectData.js
@@ -3,6 +3,7 @@ import project4 from '../Project-img/project4.jpeg'
 import project5 from '../Project-img/project5.jpeg'
 import project6 from '../Project-img/project6.jpeg'
 import project8 from '../Project-img/project8.jpeg'
+import project9 from '../Project-img/project9.png'
 
 const ProjectData=[
     {
@@ -18,21 +19,27 @@ const ProjectData=[
         link:"https://meek-smakager-529777.netlify.app/"
     },
     {
+        projectName:'Ecommerce Store',
+        projectImage:project9,
+        id:5,
+        link:"https://meek-sunshine-69d17a.netlify.app/"
+    },
+    {
         projectName:'College Website',
         projectImage:project6,
-        id:5,
+        id:6,
         link:"https://classy-donut-95598d.netlify.app/"
     },
     {
         projectName:'Tic Tac Toe',
         projectImage:project4,
-        id:6,
+        id:7,
         link:"https://creative-yeot-d51fe9.netlify.app/"
     },
     {
         projectName:'Qr Code Generator',
         projectImage:project5,
-        id:7,
+        id:8,
         link:"https://storied-hummingbird-4ff62a.netlify.app/"
     }
 ]
